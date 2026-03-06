4a97e599a8576fb99ff196904c634d1b5eadb194
ppuri1-18 <ppuri1@ualberta.ca>
2026-03-05 17:38:38 -0700

    failed hasCtiy

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    public void  testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+
+        list.addCtiy(calgary);
+
+        assertTrue(list.hasCity(calgary));
+    }
+
 }
